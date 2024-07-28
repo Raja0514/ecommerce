bb8cdf866879a7457427b9a672527d933dcdccbb
raja <rajamr9187@gmail.com>
2024-07-28 18:31:03 +0400

    final changes done

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import "vite/modulepreload-polyfill";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// vite.config.js
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "/path/to/main.js",
-    },
-  },
-});
+  plugins: [react()],
+})
